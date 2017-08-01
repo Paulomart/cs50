7d3abc39778233f4085f33ca3b3e0ce7f9b7cd58
Paul <paul.heidenreich@5minds.de>
2017-08-01 08:49:56 +0000

    .

---
+++ pset1/mario.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void) {
+
+    int height = -1;
+    do {
+        printf("Height: ");
+        height = get_int();
+    } while (height < 0 || height > 23);
+    
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < height; x++) {
+            if (height - x - 1 <= y) {
+                printf("#");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("  ");
+        for (int x = 0; x < height; x++) {
+            if (x <= y) {
+                printf("#");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+}
